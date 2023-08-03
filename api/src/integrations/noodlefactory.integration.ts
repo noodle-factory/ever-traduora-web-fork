cd354be5dbc94516cd18366138b77690b7532013
Matthew Coulter <53892067+mattcoulter7@users.noreply.github.com>
2023-08-03 22:34:21 +1000

    Add Noodle Factory Locales Client

---
+++ api/src/integrations/noodlefactory.integration.ts
@@ -0,0 +1,44 @@
+import { config } from "../config";
+import BaseIntegration from "./base.integration";
+import axios from "axios";
+
+class NoodleFactoryIntegration extends BaseIntegration {
+  baseUrl: string = config.integrations.noodlefactory.baseUrl;
+
+  async fetchData(endpoint: string) {
+    try {
+      const response = await axios.get(`${this.baseUrl}${endpoint}`);
+      console.log(response)
+      return response.data;
+    } catch (error) {
+      throw new Error(`Error fetching data: ${error.message}`);
+    }
+  }
+
+  async getProjects() {
+    const data = await this.fetchData("/locales/projects");
+    return data.data.projects;
+  }
+
+  async getLanguages() {
+    const data = await this.fetchData("/locales/languages");
+    return data.data.languages;
+  }
+
+  async getTerms() {
+    const data = await this.fetchData("/locales/terms");
+    return data.data.terms;
+  }
+
+  async getStats() {
+    const data = await this.fetchData("/locales/stats");
+    return data.data.stats;
+  }
+
+  async getTranslation(language_code: string) {
+    const data = await this.fetchData(`/locales/${language_code}`);
+    return data.data;
+  }
+}
+
+export default new NoodleFactoryIntegration();
